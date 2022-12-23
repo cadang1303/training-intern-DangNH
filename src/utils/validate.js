c9dc892f9176d56ab4b288eb254a0b5aeeadc4ba
cadang1303 <dangnguyen130399@gmail.com>
2022-12-23 08:56:02 +0700

    fix conflict #3

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -33,14 +33,11 @@ export function validateFileSize(file, maxSize) {
 }
 
 export function validateDuplicate(file, fileList) {
-  let result = false;
   let list = fileList.filter((f) => {
     return f.name != file.name;
   });
-  if (list.length === fileList.length) {
-    result = false;
-  } else result = true;
-  return result;
+
+  return !(fileList.length === list.length);
 }
 
 export function returnFileSize(number) {
